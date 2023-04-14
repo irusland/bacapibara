800aff50f675c491c740e0b81161f90b4d5bbc4b
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2023-04-15 00:01:37 +0300

    add before after

--- ts_explains.sql
+++ ts_explains.sql
@@ -4,6 +4,25 @@ EXPLAIN ANALYZE SELECT *
 FROM messages
 WHERE to_tsvector('russian', messages.text) @@ to_tsquery( 'russian', 'рай' );
 
+-- BEFORE INDEX
+--                                                            QUERY PLAN
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Gather  (cost=1000.00..197442.36 rows=6642 width=275) (actual time=37.070..6518.188 rows=44760 loops=1)
+--   Workers Planned: 2
+--   Workers Launched: 2
+--   ->  Parallel Seq Scan on messages  (cost=0.00..195778.16 rows=2768 width=275) (actual time=15.712..6439.098 rows=14920 loops=3)
+--         Filter: (to_tsvector('russian'::regconfig, (text)::text) @@ '''ра'''::tsquery)
+--         Rows Removed by Filter: 428095
+-- Planning Time: 0.830 ms
+-- JIT:
+--   Functions: 6
+--   Options: Inlining false, Optimization false, Expressions true, Deforming true
+--   Timing: Generation 13.704 ms, Inlining 0.000 ms, Optimization 12.631 ms, Emission 29.876 ms, Total 56.211 ms
+-- Execution Time: 6534.349 ms
+--(12 rows)
+--
+
+-- AFTER INDEX
 --                                                                QUERY PLAN
 -------------------------------------------------------------------------------------------------------------------------------------------
 -- Gather  (cost=1420.21..61767.92 rows=44414 width=275) (actual time=24.001..97.321 rows=44759 loops=1)
@@ -25,6 +44,25 @@ EXPLAIN ANALYZE SELECT users.name, users.age, users.about, users.email, users.pa
 FROM users
 WHERE to_tsvector('russian', name || ' ' || about) @@ to_tsquery('russian', 'Руслан');
 
+-- BEFORE INDEX
+--                                                          QUERY PLAN
+--------------------------------------------------------------------------------------------------------------------------------
+-- Gather  (cost=1000.00..129431.28 rows=4740 width=151) (actual time=27.742..1336.049 rows=1489 loops=1)
+--   Workers Planned: 2
+--   Workers Launched: 2
+--   ->  Parallel Seq Scan on users  (cost=0.00..127957.28 rows=1975 width=151) (actual time=14.920..1278.123 rows=496 loops=3)
+--         Filter: (to_tsvector('russian'::regconfig, (((name)::text || ' '::text) || (about)::text)) @@ '''русла'''::tsquery)
+--         Rows Removed by Filter: 315507
+-- Planning Time: 1.901 ms
+-- JIT:
+--   Functions: 6
+--   Options: Inlining false, Optimization false, Expressions true, Deforming true
+--   Timing: Generation 3.987 ms, Inlining 0.000 ms, Optimization 7.763 ms, Emission 17.743 ms, Total 29.494 ms
+-- Execution Time: 1339.010 ms
+--(12 rows)
+
+
+-- AFTER INDEX
 --                                                             QUERY PLAN
 ---------------------------------------------------------------------------------------------------------------------------------------
 -- Bitmap Heap Scan on users  (cost=968.46..3042.24 rows=576 width=150) (actual time=1.564..25.414 rows=945 loops=1)
